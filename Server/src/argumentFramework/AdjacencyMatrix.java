a4bf862f6fe7a00d9b5f6c5ec03322389bc264db
YutakaOmido <db373db@gmail.com>
2014-11-09 19:44:11 +0900

    隣接行列クラスで、要素のゲッタ・セッタと行と列の追加を実装

--- Server/src/argumentFramework/AdjacencyMatrix.java
+++ Server/src/argumentFramework/AdjacencyMatrix.java
@@ -7,11 +7,21 @@ public class AdjacencyMatrix {
 	private int rowSize;
 	private int columnSize;
 
-	public AdjacencyMatrix(){
+	public AdjacencyMatrix() {
+		this(0, 0);
+	}
+
+	public AdjacencyMatrix(int rowSize, int columnSize) {
 		this.matrix = new ArrayList<ArrayList<Boolean>>();
 		this.rowSize = 0;
 		this.columnSize = 0;
+
+		for(int i=0; i<rowSize; i++){ addRowAt(this.rowSize); }
+		for(int i=0; i<columnSize; i++){ addColumnAt(this.columnSize); }
 	}
+
+	public Boolean getValue(int row, int column) { return this.matrix.get(row).get(column); }
+	public void setValue(int row, int column, boolean value) { this.matrix.get(row).set(column, value); }
 
 	public void addRowAt(int index){
 		if(index <= this.rowSize){
@@ -32,7 +42,7 @@ public class AdjacencyMatrix {
 		if(index <= this.columnSize){
 			for(int i=0; i<this.rowSize; i++){
 				ArrayList<Boolean> row = this.matrix.get(i);
-				row.add(false);
+				row.add(index, false);
 			}
 			this.columnSize++;
 		}
@@ -43,6 +53,7 @@ public class AdjacencyMatrix {
 	}
 
 	public String toString(){
+		System.out.println(this.rowSize + " : " + this.columnSize);
 		StringBuffer sb = new StringBuffer();
 		for(int i=0; i<this.rowSize; i++){
 			for(int j=0; j<this.columnSize; j++){
@@ -58,14 +69,20 @@ public class AdjacencyMatrix {
 	}
 
 	public static void main(String[] args){
-		AdjacencyMatrix matrix = new AdjacencyMatrix();
+		AdjacencyMatrix matrix = new AdjacencyMatrix(2, 3);
+		matrix.setValue(0, 0, true);
+		matrix.setValue(1, 2, true);
 		System.out.println("print matrix");
 		System.out.println(matrix.toString());
 		matrix.addColumnAt(0);
 		matrix.addColumnAt(1);
-		matrix.addRowAt(0);
-		matrix.addColumnAt(0);
-		matrix.addRowAt(0);
+		matrix.addRowAt(1);
+		matrix.setValue(1, 4, true);
+		System.out.println("print matrix");
+		System.out.println(matrix.toString());
+		matrix.setValue(2, 4, false);
+		matrix.addColumnAt(3);
+		matrix.addRowAt(3);
 		System.out.println("print matrix");
 		System.out.println(matrix.toString());
 	}
